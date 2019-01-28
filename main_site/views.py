1f016fa03ac3df007f4a76177e628b05446f7859
AyrtonB <ayrtonbourn@outlook.com>
2019-01-28 18:40:45 +0000

    prod

--- main_site/views.py
+++ main_site/views.py
@@ -34,7 +34,7 @@ class highDeltaChartData(APIView):
 	permission_classes = []
 
 	def get(self, request, format=None):
-		json_filepath = os.path.join(BASE_DIR, 'static\main_site\data\high_delta_scatter.json') #os.path.abspath(os.path.dirname(__name__))
+		json_filepath = 'staticfiles/main_site/data/high_delta_scatter.json' #os.path.join(BASE_DIR, 'static\main_site\data\high_delta_scatter.json') #os.path.abspath(os.path.dirname(__name__))
 		json_data = open(json_filepath).read()
 		data = json.loads(json_data)
 		return JsonResponse(data, safe=False)
@@ -44,13 +44,13 @@ class lowDeltaChartData(APIView):
 	permission_classes = []
 
 	def get(self, request, format=None):
-		json_filepath = os.path.join(BASE_DIR, 'static\main_site\data\low_delta_scatter.json') #os.path.abspath(os.path.dirname(__name__))
+		json_filepath = 'staticfiles/main_site/data/low_delta_scatter.json' #os.path.join(BASE_DIR, 'static\main_site\data\low_delta_scatter.json') #os.path.abspath(os.path.dirname(__name__))
 		json_data = open(json_filepath).read()
 		data = json.loads(json_data)
 		return JsonResponse(data, safe=False)
 
 def get_household_data(request, *args, **kwargs):
-	json_filepath = '/static/main_site/data/household_features.json' #os.path.join(BASE_DIR, 'static\main_site\data\household_features.json') #os.path.abspath(os.path.dirname(__name__))
+	json_filepath = 'staticfiles/main_site/data/household_features.json' #os.path.join(BASE_DIR, 'static\main_site\data\household_features.json') #os.path.abspath(os.path.dirname(__name__))
 	json_data = open(json_filepath).read()
 	data = json.loads(json_data)
 
