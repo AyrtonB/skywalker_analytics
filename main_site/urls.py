2021b148b6d7e8d5d56f6efb67c98ccee15af3e4
AyrtonB <ayrtonbourn@outlook.com>
2019-01-28 16:53:49 +0000

    production_ready

--- main_site/urls.py
+++ main_site/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
-from .views import get_data, ChartData
+from .views import get_household_data, highDeltaChartData, lowDeltaChartData
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^charts/$', views.charts, name='charts'),
     url(r'^case_study/$', views.case_study, name='case_study'),
     url(r'^contact/$', views.contact, name='contact'),
-    url(r'^api/chart/data/$', ChartData.as_view()),
-    url(r'^api/data/$', get_data, name='api-data'),
+    url(r'^data/$', views.data, name='contact'),
+    url(r'^api/chart/highDeltaScatter/$', highDeltaChartData.as_view()),
+    url(r'^api/chart/lowDeltaScatter/$', lowDeltaChartData.as_view()),
+    url(r'^api/data/(?P<household_id>.+)/$', get_household_data, name='api-data'),
 ]
--- main_site/views.py
+++ main_site/views.py
@@ -8,7 +8,11 @@ from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+import json
+import os
+
 User = get_user_model()
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 def index(request):
     return render(request, 'main_site/home.html')
@@ -19,24 +23,43 @@ def case_study(request):
 def contact(request):
     return render(request, 'main_site/contact.html')
 
+def data(request):
+    return render(request, 'main_site/data.html')
+
 def charts(request):
     return render(request, 'main_site/charts.html')
 
-class ChartData(APIView):
+class highDeltaChartData(APIView):
 	authentication_classes = []
 	permission_classes = []
 
 	def get(self, request, format=None):
-		data = {
-			"sales": 100,
-			"customers": 10,
-			"users": User.objects.all().count(),
-		}
-		return JsonResponse(data)
+		json_filepath = os.path.join(BASE_DIR, 'static\main_site\data\high_delta_scatter.json') #os.path.abspath(os.path.dirname(__name__))
+		json_data = open(json_filepath).read()
+		data = json.loads(json_data)
+		return JsonResponse(data, safe=False)
 
-def get_data(request, *args, **kwargs):
-	data = {
-		"sales": 100,
-		"customers": 10,
-	}
-	return JsonResponse(data)
+class lowDeltaChartData(APIView):
+	authentication_classes = []
+	permission_classes = []
+
+	def get(self, request, format=None):
+		json_filepath = os.path.join(BASE_DIR, 'static\main_site\data\low_delta_scatter.json') #os.path.abspath(os.path.dirname(__name__))
+		json_data = open(json_filepath).read()
+		data = json.loads(json_data)
+		return JsonResponse(data, safe=False)
+
+def get_household_data(request, *args, **kwargs):
+	json_filepath = os.path.join(BASE_DIR, 'static\main_site\data\household_features.json') #os.path.abspath(os.path.dirname(__name__))
+	json_data = open(json_filepath).read()
+	data = json.loads(json_data)
+
+	household_id = kwargs['household_id']
+
+	try:
+		data_out = data[household_id]
+		print(data_out)
+	except:
+		data_out = {'status':'The household ID you gave is unavailable'}
+
+	return JsonResponse(data_out, safe=False)
